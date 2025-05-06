fc770d8e0dc4303c3320b982eeb222b675921592
almaeq <alma.quinterosquiroga@gmail.com>
2025-05-06 13:36:18 -0300

    add Prestamo Controller comments

--- src/main/java/com/example/springLibrarySystem/controller/PrestamoController.java
+++ src/main/java/com/example/springLibrarySystem/controller/PrestamoController.java
@@ -18,12 +18,21 @@ public class PrestamoController {
         this.prestamoService = prestamoService;
     }
 
+    /**
+     * Obtiene la lista completa de prestamos registrados.
+     * @return Lista de prestamos
+     */
     // GET /api/prestamos
     @GetMapping
     public ResponseEntity<List<Prestamo>> getAll() {
         return ResponseEntity.ok(prestamoService.findAll());
     }
 
+    /**
+     * Obtiene un prestamo por su ID.
+     * @param id Identificador del préstamo
+     * @return Prestamo correspondiente al ID
+     */
     // GET /api/prestamos/{id}
     @GetMapping("/{id}")
     public ResponseEntity<Prestamo> getById(@PathVariable Long id) {
@@ -35,12 +44,23 @@ public class PrestamoController {
         }
     }
 
+    /**
+     * Registra un nuevo préstamo.
+     * @param prestamo Datos del prestamo a registrar
+     * @return Prestamo registrado con ID asignado
+     */
     // POST /api/prestamos
     @PostMapping
     public ResponseEntity<Prestamo> create(@RequestBody Prestamo prestamo) {
         return ResponseEntity.status(HttpStatus.CREATED).body(prestamoService.save(prestamo));
     }
 
+    /**
+     * Actualiza un prestamo existente.
+     * @param id ID del libro a actualizar
+     * @param prestamo Nuevos datos del libro
+     * @return prestamo actualizado
+     */
     // PUT /api/prestamos/{id}
     @PutMapping("/{id}")
     public ResponseEntity<Prestamo> update(@PathVariable Long id, @RequestBody Prestamo prestamo) {
@@ -52,6 +72,11 @@ public class PrestamoController {
         }
     }
 
+    /**
+     * Elimina un prestamo por su ID.
+     * @param id ID del prestamo
+     * @return Sin contenido si se elimina correctamente
+     */
     // DELETE /api/prestamos/{id}
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
